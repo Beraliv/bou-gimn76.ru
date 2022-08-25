5123a6d1e8c7a62191ce0b831ef3ebd1893fcd88
Alexey Berezin <beraliv.spb@gmail.com>
2022-08-25 23:57:26 +0100

    feat: ➕ add production heroku postgres

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,10 +1,15 @@
-import path from 'path';
+import { join } from "path";
 
 export default ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "sqlite",
     connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: join(
+        __dirname,
+        "..",
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
     useNullAsDefault: true,
   },
